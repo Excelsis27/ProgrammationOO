d1432e2fb825c38e9b75165ebb9f7b8b10d19d1d
Excelsis27 <andre.lapointe@outlook.com>
2021-02-01 12:25:41 -0500

    Cours04

--- ProgrammationOO/IntroACSharp/Program.cs
+++ ProgrammationOO/IntroACSharp/Program.cs
@@ -7,9 +7,11 @@ class Program
     {
         //TestDeBase();
         //TestDeControles();
-        TestDeValeurs();
+        //TestDeValeurs();
+        //TestTableau();
+        TestChaineDeCaracteres();
 
-        Console.WriteLine("Appuyez sur une touche pour continuer");
+        Console.Write("Appuyez sur une touche pour continuer");
         Console.ReadKey();
     }
 
@@ -122,21 +124,23 @@ class Program
 
     static void TestDeValeurs()
     {
-        int nombre1 = 10;
-        int nombre2 = 20;
-        int nombre3 = 30;
+        int nombre1;
+        int nombre2;
+        int nombre3;
 
-        //ObtenirValeurs(ref nombre1, ref nombre2, ref nombre3);
+        ObtenirValeurs(out nombre1, out nombre2, out nombre3);
         Console.WriteLine("Valeurs d'origine: " + nombre1 + ", " + nombre2 + ", " + nombre3);
 
         ModifierValeurs(ref nombre1, ref nombre2, ref nombre3);
         Console.WriteLine("Valeurs modifiées: " + nombre1 + ", " + nombre2 + ", " + nombre3);
     }
-    static void ObtenirValeurs(ref int valeur1, ref int valeur2, ref int valeur3)
+    static void ObtenirValeurs(out int valeur1, out int valeur2, out int valeur3)
     {
         valeur1 = 17;
         valeur2 = 42;
-        // valeur3 n'est initialisée volontairement
+        // ------valeur3 n'est initialisée volontairement------
+        // Avec out comme type de référence, il faut initialiser toutes les variables
+        valeur3 = 49;
     }
 
     static void ModifierValeurs(ref int valeur1, ref int valeur2, ref int valeur3)
@@ -149,4 +153,99 @@ class Program
         valeur3 = (valeur2 + 3) % 7;
     }
 
+    static void TestTableau()
+    {
+        int[] tableauEntiers = { 0, 1, 2, 3, 5, 8, 13 }; // Un tableau de 7 éléments
+
+        tableauEntiers[0] = 1;
+
+        Console.WriteLine("Le tableau contient {0} éléments", tableauEntiers.Length);
+        Console.WriteLine("Le premier élément est: " + tableauEntiers[0]);
+        Console.WriteLine("Le dernier élément est: " + tableauEntiers[tableauEntiers.Length - 1]);
+
+        // Va faire planter le programme
+        //Console.WriteLine("Élément après le dernier: " + tableauEntiers[ tableauEntiers.Length]); // Out of range, ne fonctionne pas
+        //tableauEntiers[12] = 21; //pareil ici
+
+        for (int i = 0; i < tableauEntiers.Length; i++)
+        {
+            Console.Write(tableauEntiers[i] + ", ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Boucle foreach");
+        foreach (int item in tableauEntiers)
+        {
+            Console.Write(item + ", ");
+        }
+        Console.WriteLine();
+
+
+        // Tableau de chaines de caractrères
+        string[] nomsMois = { "Janvier", "Fébrier", "Mars", "Mai" };
+        Console.WriteLine("Le 2e mois est: " + nomsMois[1]);
+        nomsMois[3] = "Avril";
+
+        foreach (string item in nomsMois)
+        {
+            Console.WriteLine(item + ", ");
+        }
+        Console.WriteLine();
+    }
+
+    static void TestChaineDeCaracteres()
+    {
+        string maChaine = "Bonjour";
+
+        // Length est disponible pour les strings
+        Console.WriteLine("Longueur de la chaine: " + maChaine.Length + ", le 1er caractère est '" + maChaine[0] + "'" + "et le dernier caractère est '" + maChaine[maChaine.Length - 1] + "'");
+
+        foreach (char item in maChaine)
+        {
+            Console.WriteLine(item);
+        }
+
+        // Impossible de modifier le contenu d'une chaine individuellement
+        //maChaine[1] = 'a';
+
+        // On peut remplacer la chaine au complet comme ceci
+        maChaine = "Ceci est";
+        maChaine += " un tes";
+        maChaine += 't'; // += fonctionne avec un seul caractère
+
+        // Divise la chaine en sous-chaines à chaque occurence du caractère donné
+        string[] mots = maChaine.Split(' ');
+        foreach (string mot in mots)
+        {
+            Console.WriteLine(mot);
+        }
+
+        maChaine = "            Une chaine de caractères avec des espaces avant et après.           ";
+        Console.WriteLine("Chaine originale: \"{0}\"", maChaine);
+        // Trim enlève les espaces au début et à la fin + TrimStart/TrimEnd
+        maChaine.Trim();
+        Console.WriteLine("Chaine sans espaces: \"{0}\"", maChaine);
+        maChaine = maChaine.Trim();
+        Console.WriteLine("Chaine sans espaces (prise 2): \"{0}\"", maChaine);
+
+        string aChercher = "Une";
+        if (maChaine.Contains(aChercher))
+        {
+            Console.WriteLine("La chaine contient " + aChercher);
+
+            if (maChaine.StartsWith(aChercher))
+            {
+                Console.WriteLine("La chaine débute par " + aChercher);
+            }
+            else
+            {
+                Console.WriteLine("La chaine ne débute pas par " + aChercher);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("La chaine ne contient pas " + aChercher);
+        }
+    }
 }
