198338a94d88c842ea06ff77b2977187f9c28a5a
Excelsis27 <andre.lapointe@outlook.com>
2021-02-17 16:04:18 -0500

    Cours11

--- ProgrammationOO/IntroPOO/Banque.cs
+++ ProgrammationOO/IntroPOO/Banque.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace IntroPOO
 {
@@ -7,25 +8,78 @@ namespace IntroPOO
     /// </summary>
     class Banque
     {
-        public Banque(string fichier)
+        public Banque(string nomFichier)
         {
+            using (StreamReader fichierLecture = new StreamReader(nomFichier))
+            {
+                // Pas de traitement d'erreur, le fichier contiendra toujours 5 lignes valides
+                for (int i = 0; i < NbComptes; ++i)
+                {
+                    string ligne = fichierLecture.ReadLine();
+
+                    _comptes[i] = new CompteBancaire(ligne);
+                }
+            }
         }
 
         public void ListerComptes()
         {
+            foreach (CompteBancaire item in _comptes)
+            {
+                item.Afficher();
+            }
         }
-        public void AfficherSolde(string nom)
+        public void AfficherSolde(string nomCompte)
         {
+            CompteBancaire compte = rechercherCompte(nomCompte);
+            if (compte != null)
+            {
+                // On a trouvé le bon compte
+                compte.AfficherSolde();
+            }
         }
-        public void Deposer(string nom, double montant)
+        public void Deposer(string nomCompte, double montant)
         {
+            CompteBancaire compte = rechercherCompte(nomCompte);
+            if (compte != null)
+            {
+                // On a trouvé le bon compte
+                compte.Deposer(montant);
+            }
         }
-        public void Retirer(string nom, double montant)
+        public void Retirer(string nomCompte, double montant)
         {
+            CompteBancaire compte = rechercherCompte(nomCompte);
+            if (compte != null)
+            {
+                // On a trouvé le bon compte
+                compte.Retirer(montant);
+            }
         }
         public void Sauvegarder()
         {
         }
+
+        // Méthode privée car uniquement utilisée par la classe
+        // Recherche le compte donné dans le tableau
+        // Affiche une erreur si le compte est introvable
+        // Retourne le compte trouvé, ou null si le compte est introuvable
+        private CompteBancaire rechercherCompte(string nomCompte)
+        {
+            foreach (CompteBancaire item in _comptes)
+            {
+                if (item.EstEgaleA(nomCompte))
+                {   
+                    // On a trouvé le compte
+                    return item;
+                }
+            }
+
+            // Si on arrive ici, on a effectué toute la boucle sans trouver le compte
+            Console.WriteLine("Le compte '{0}' n'existe pas", nomCompte);
+            return null;
+        }
+
 
         private const int NbComptes = 5;
 
@@ -35,6 +89,6 @@ namespace IntroPOO
         //   _comptes[0] = new CompteBancaire(...);
         //   ...
         //   _comptes[4] = new CompteBancaire(...);
-        private CompteBancaire[] _comptes = new CompteBancaire[NbComptes];
+        private CompteBancaire[] _comptes = new CompteBancaire[NbComptes]; // 5 comptes à null
     }
 }
--- ProgrammationOO/IntroPOO/CompteBancaire.cs
+++ ProgrammationOO/IntroPOO/CompteBancaire.cs
@@ -8,5 +8,76 @@ namespace IntroPOO
 {
     class CompteBancaire
     {
+
+        public CompteBancaire(string ligneFichier)
+        {
+            string[] elements = ligneFichier.Split(';');
+
+            _type = elements[0];
+            _nom = elements[1]; // readonly, doit être initialisé dans le constructeur
+            _solde = Convert.ToDouble(elements[2]);
+        }
+        /// <summary>
+        /// Affiche l'imformation du compte
+        /// </summary>
+        public void Afficher()
+        {
+            // Impossible de modifier un attribut readonly
+            // _nom = "Nouveau nom";
+            Console.WriteLine("Compte {0}: {1}", _type, _nom);
+        }
+
+        /// <summary>
+        /// Indique si le compte porte le nom donné
+        /// </summary>
+        /// <param name="nom">Le nom à valider</param>
+        /// <returns>Vrai si le nom correspond, faux sinon</returns>
+        public bool EstEgaleA(string nom)
+        {
+            return nom == _nom;
+        }
+
+        /// <summary>
+        /// Affiche le solde du compte
+        /// </summary>
+        public void AfficherSolde()
+        {
+            // Format C = Currency (Devise: $)
+            Console.WriteLine("Solde du compte '{0}': {1:C}", _nom, _solde);
+        }
+
+        /// <summary>
+        /// Effectue un dépot dans le compte
+        /// </summary>
+        /// <param name="montant">Montant à déposer</param>
+        public void Deposer(double montant)
+        {
+            // On ajoute le montant au solder
+            _solde += montant;
+            Console.WriteLine("Dépot de {0:C} dans le compte {1}, nouveau solder: {2:C}", montant, _nom, _solde);
+        }
+        /// <summary>
+        /// Effectue un retrait dans le compte
+        /// Affiche un message d'erreur si le solde est insuffisant. Le solde reste alors inchangé
+        /// </summary>
+        /// <param name="montant">Montant à retirer</param>
+        public void Retirer(double montant)
+        {
+            if (montant <= _solde)
+            {
+                // Le solde est suffisant
+                _solde -= montant;
+                Console.WriteLine("Retrait de {0:C} du compte {1}, nouveau solde: {2:C}", montant, _nom, _solde);
+            }
+            else
+            {
+                Console.WriteLine("Retrait impossible, solder insuffisant");
+            }
+        }
+
+        // Le type et le nom ne peuvent pas être modifiés après la construction de l'objet
+        private readonly string _type;
+        private readonly string _nom;
+        private double _solde;
     }
 }
