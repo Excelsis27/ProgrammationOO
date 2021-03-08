e80cc783f9fe49328dd4c78fa3192ec81eaf7c95
Excelsis27 <andre.lapointe@outlook.com>
2021-03-08 12:26:23 -0500

    Cours16

---
+++ ProgrammationOO/Listes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic; // Pour la classe List
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestListeEntiers();
+
+
+            Pause();
+        }
+
+        static void TestListeEntiers()
+        {
+            // Le type List est un type générique
+            // Il faut spécifier le type contenu dans la liste avec la syntaxe: List<TYPE>
+            List<int> listeEntiers = new List<int>();
+
+            Random generateurAleatoire = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                // Add ajoute à la fin de la liste
+                listeEntiers.Add(generateurAleatoire.Next(100));
+            }
+
+            // Count indique le nombre d'éléments dans la liste
+            Console.WriteLine("Taille de la liste: " + listeEntiers.Count);
+
+            foreach (var item in listeEntiers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Pause()
+        {
+            Console.WriteLine("Appuyez sur une touche pour continuer...");
+            Console.ReadKey(true);
+        }
+    }
+}
