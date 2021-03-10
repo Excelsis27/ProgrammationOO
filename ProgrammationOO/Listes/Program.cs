5bbecde7e27722545fb5652c504b21ba6d2e2fc5
Excelsis27 <andre.lapointe@outlook.com>
2021-03-10 16:07:24 -0500

    Cours17

--- ProgrammationOO/Listes/Program.cs
+++ ProgrammationOO/Listes/Program.cs
@@ -10,8 +10,9 @@ namespace Listes
     {
         static void Main(string[] args)
         {
-            TestListeEntiers();
-
+            //TestListeEntiers();
+            //TestListeStrings();
+            TestListeDates();
 
             Pause();
         }
@@ -34,6 +35,126 @@ namespace Listes
             Console.WriteLine("Taille de la liste: " + listeEntiers.Count);
 
             foreach (var item in listeEntiers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Premier élément: " + listeEntiers[0]);
+            Console.WriteLine("Dernier élément: " + listeEntiers[listeEntiers.Count- 1]);
+            listeEntiers[2] = 100;
+
+
+            // Trier la liste en ordre croissant
+            listeEntiers.Sort();
+
+            Console.WriteLine("En ordre croissant: " + listeEntiers);
+            foreach (var item in listeEntiers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void TestListeStrings()
+        {
+            List<string> listeStrings = new List<string>() { "Un", "Trois", "Quatre", "Trois", "Quatre" };
+            AfficherListeStrings(listeStrings);
+
+            Console.WriteLine("Insertion de \"Deux\" à la position 1 (2e)");
+            listeStrings.Insert(1, "Deux");
+            AfficherListeStrings(listeStrings);
+
+            // Insert(0,...) insère au début de la liste
+            Console.WriteLine("Insertion de \"Zéro\" à la position 0 (1e)");
+            listeStrings.Insert(0, "Zéro");
+            AfficherListeStrings(listeStrings);
+
+            int index = listeStrings.IndexOf("Trois");
+            Console.WriteLine("Première position de \"Trois\": " + index);
+            Console.WriteLine("Prochaine position de \"Trois\": " + listeStrings.IndexOf("Trois", index + 1));
+            Console.WriteLine("Dernière position de \"Trois\": " + listeStrings.LastIndexOf("Trois"));
+            // -1 si la valeur n'est pas trouvé
+            Console.WriteLine("Position de \"Cinq\" : " + listeStrings.IndexOf("Cinq"));
+
+            if (listeStrings.Contains("Trois"))
+            {
+                Console.WriteLine("La liste contient \"Trois\".");
+            }
+
+            Console.WriteLine("Enlever la première occurence de \"Quatre\" ");
+            listeStrings.Remove("Quatre");
+            AfficherListeStrings(listeStrings);
+            Console.WriteLine("Élément à la position 4: " + listeStrings[4]);
+            Console.WriteLine("Enlève l'élément à la position 4");
+            listeStrings.RemoveAt(4);
+            AfficherListeStrings(listeStrings);
+
+            // Trier la liste en ordre décroissant
+            // Tri en ordre croissant
+            listeStrings.Sort();
+            // ... puis on inverse la liste
+            listeStrings.Reverse();
+
+            Console.WriteLine("En ordre décroissant: " + listeStrings);
+            AfficherListeStrings(listeStrings);
+
+            Console.WriteLine("On vid la liste");
+            listeStrings.Clear();
+            AfficherListeStrings(listeStrings);
+        }
+
+
+        static void AfficherListeStrings(List<string> listeString)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Taille de la liste: " + listeString.Count);
+            foreach (var item in listeString)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("------------------");
+        }
+
+
+        static void TestListeDates()
+        {
+            List<DateTime> listeDate = new List<DateTime>()
+            {
+                // Année, Mois, Jour
+                new DateTime(2020, 7, 21),
+                new DateTime(1988, 4, 26),
+                new DateTime(2005, 3, 5),
+                // Année, Mois, Jour, Heure, Minute, Seconde
+                new DateTime(1984, 4, 1, 10, 32, 55)
+            };
+
+            // La propriété statique Now retourne un objet DateTime initialisé à l'heure actualle de l'ordinateur
+            DateTime maintenant = DateTime.Now;
+            Console.WriteLine(maintenant);
+            Console.WriteLine("---------");
+
+            listeDate.Add(maintenant);
+            DateTime aujourdhui = DateTime.Today;
+            Console.WriteLine(aujourdhui);
+            Console.WriteLine("---------");
+
+            listeDate.Add(aujourdhui);
+
+            try
+            {
+                Console.Write("Indiquez une date (A/M/J): ");
+                DateTime date = Convert.ToDateTime(Console.ReadLine());
+                Console.WriteLine(" Date courte: " + date.ToShortDateString());
+                Console.WriteLine(" Date longue: " + date.ToLongDateString());
+                listeDate.Add(date);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            listeDate.Sort();
+
+            foreach (var item in listeDate)
             {
                 Console.WriteLine(item);
             }
